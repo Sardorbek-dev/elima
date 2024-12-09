d96b8a04f1652aa25a577b7bb2fde05a23d8bde1
Sardorbek-dev <sardorsoftwaredeveloper@gmail.com>
2024-12-09 19:35:20 +0100

    fix slug issue

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
+from unidecode import unidecode
 
 
 class PostCategory(models.Model):
@@ -23,11 +24,11 @@ class PostCategory(models.Model):
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=255, blank=False)
+    slug = models.SlugField(unique=True, blank=True)
     content = RichTextField(config_name='default')
     author = models.CharField(max_length=100)
-    photo = models.ImageField(upload_to='img/', blank=True, null=True)
+    photo = models.ImageField(upload_to='img/')
     created_at = models.DateTimeField(default=timezone.now)
     show_on_homepage = models.BooleanField(default=False)
     publish = models.BooleanField(default=True)
@@ -38,6 +39,9 @@ class Post(models.Model):
         verbose_name_plural = _("Posts")
 
     def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
+        if not self.slug and self.title:
+            print("Generating slug for:", self.title)
+            self.slug = slugify(unidecode(self.title))
+        else: 
+            print("Slug already exists or title is missing")
         super().save(*args, **kwargs)
